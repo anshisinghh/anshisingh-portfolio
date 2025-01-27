bf526e6032b5566e99c5f2a42c423ee33dfe63ff
Anshi Singh <anshisingh730@gmail.com>
2025-01-27 00:27:59 -0800

    Update Contact.tsx

--- animated-portfolio/app/components/Contact.tsx
+++ animated-portfolio/app/components/Contact.tsx
@@ -29,7 +29,11 @@ const Contact = () => {
 
   return (
     <Reveal initialY={40} delay={0.5}>
-      <section ref={ref} id="contact" className=" card relative items-center mx-6 flex flex-col px-[33px] py-[27px] z-30 gap-[54px] md:gap-[35px] mb-[67px] md:mb-[42px] group overflow-hidden">
+      <section 
+        ref={ref} 
+        id="contact" 
+        className=" card relative items-center mx-6 flex flex-col px-[33px] py-[27px] z-30 gap-[54px] md:gap-[35px] mb-[67px] md:mb-[42px] group overflow-hidden"
+      >
         <div className="flex flex-col gap-5 items-center">
           <h2 className="font-semibold text-[22px] md:text-[40px] md:max-w-[462px]">Get in Touch</h2>
           <div className="highlight text-xl">Irvine, CA | anshisingh730@gmail.com | (949) 353-4274</div>
