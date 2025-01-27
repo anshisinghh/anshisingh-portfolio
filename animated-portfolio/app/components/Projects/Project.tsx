79579de083ce650b649a8ac95dad7bb249002521
Anshi Singh <anshisingh730@gmail.com>
2025-01-26 18:53:31 -0800

    projects & skills sections

---
+++ animated-portfolio/app/components/Projects/Project.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from 'next/image'
+
+type Props = {
+  thumbnail: string;
+  title: string;
+  link: {
+    url: string;
+    label: string;
+  }
+  description: string;
+  languageIcons: string[];
+}
+
+const Project = ({thumbnail, title, link, description, languageIcons}: Props) => {
+  return (
+    <div className="card flex flex-col items-stretch w-full max-w=[428.4px] p-5 md:p-[18px] gap-[30px]">
+      <Image src={thumbnail} alt={`Thumbnail for ${title}`} width={392} height={230} className="w-full h-[230px] object-cover"></Image>
+      <div className="flex flex-col gap-[11px]">
+        <div className="flex justify-between items-center">
+          <h3 className="font-semibold text-xl md:text-[22px]">{title}</h3>
+          <a href={link.url} target="_blank" rel="noopener noreferrer" className="flex px-[5px] py-[3px] gap-1 bg-[#b9b9b9] bg-opacity-[24%] rounded text-[14px]">
+            <span className="hidden md:block">{link.label}</span>
+            <img src="link_arrow.svg" alt="Link arrow" className="block dark:hidden"></img>
+            <img src="link_arrow_dark.svg" alt="Link arrow" className="hidden dark:block"></img>
+          </a>
+        </div>
+      </div>
+      <p className="line-clamp-2 text-sm md:text-base">{description}</p>
+      <div className="flex flex-row gap-[11px] ">
+        {languageIcons.map((icon, iconId) => (
+          <img src={icon} alt="Language icon" key={iconId}></img>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Project
--- animated-portfolio/app/components/Sections.tsx
+++ animated-portfolio/app/components/Sections.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import About from './About/About'
+import Projects from './Projects/Projects'
+import Skills from './Skills/Skills'
 
 const Sections = () => {
   return (
     <main className="flex flex-col gap-[142px] w-full md:max-w-screen-lg pt-[236px] md:pt-60 mx-auto">
         <About />
+        <Projects />
+        <Skills />
     </main>
   )
 }
