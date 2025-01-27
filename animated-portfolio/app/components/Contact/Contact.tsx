589fca31de0e6600f9b899d081fc7dc58e7ea0a8
Anshi Singh <anshisingh730@gmail.com>
2025-01-26 21:03:40 -0800

    reusable components for links

--- animated-portfolio/app/components/Contact/Contact.tsx
+++ animated-portfolio/app/components/Contact/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaEnvelope, FaLinkedin, FaFilePdf } from 'react-icons/fa';
+import SocialLinks from '../SocialLinks/SocialLinks';
 
 const Contact = () => {
   return (
@@ -7,20 +7,7 @@ const Contact = () => {
       <div className="flex flex-col gap-5 items-center">
         <h2 className="font-semibold text-[22px] md:text-[40px] md:max-w-[462px]">Get in Touch</h2>
         <div className="highlight">Irvine, CA | anshisingh730@gmail.com | (949) 353-4274</div>
-        <div className="flex flex-row gap-5 mt-4">
-        <a href="https://github.com/anshisinghh" title="Github" target="_blank" rel="noopener noreferrer" className="text-xl md:text-2xl text-white">
-            <FaGithub />
-        </a>
-        <a href="mailto:anshisingh730@gmail.com" title="Email" className="text-xl md:text-2xl text-white">
-            <FaEnvelope />
-        </a>
-        <a href="https://linkedin.com/in/anshisingh" title="LinkedIn" target="_blank" rel="noopener noreferrer" className="text-xl md:text-2xl text-white">
-            <FaLinkedin />
-        </a>
-        <a href="/Anshi_Singh_Resume.pdf" title="Resume" target="_blank" rel="noopener noreferrer" className="text-xl md:text-2xl text-white">
-            <FaFilePdf />
-        </a>
-        </div>
+        <SocialLinks />
       </div>
       <small>Copyright &copy; Anshi Singh 2025</small>
     </section>
