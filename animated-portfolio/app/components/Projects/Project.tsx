378a2ec201615640ae4ab736c752d8f2e75d3dd7
Anshi Singh <anshisingh730@gmail.com>
2025-01-26 22:52:38 -0800

    fixed styling

--- animated-portfolio/app/components/Projects/Project.tsx
+++ animated-portfolio/app/components/Projects/Project.tsx
@@ -25,7 +25,7 @@ const Project = ({thumbnail, title, link, description, languageIcons}: Props) =>
           </a>
         </div>
       </div>
-      <p className="line-clamp-2 text-sm md:text-base">{description}</p>
+      <p className="text-sm md:text-base">{description}</p>
       <div className="flex flex-row gap-[11px] ">
         {languageIcons.map((icon, iconId) => (
           <img src={icon} alt="Language icon" key={iconId}></img>
