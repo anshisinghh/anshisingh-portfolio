218e82fc063848284eade45ac0f1ee06c8c9562a
Anshi Singh <anshisingh730@gmail.com>
2025-01-27 19:55:07 -0800

    removed card from contact

--- animated-portfolio/app/components/Contact.tsx
+++ animated-portfolio/app/components/Contact.tsx
@@ -32,7 +32,7 @@ const Contact = () => {
       <section
         ref={ref}
         id="contact"
-        className=" card relative items-center mx-6 flex flex-col px-[33px] py-[27px] z-30 gap-[54px] md:gap-[35px] mb-[67px] md:mb-[42px] group overflow-hidden"
+        className="relative items-center mx-6 flex flex-col px-[33px] py-[27px] z-30 gap-[54px] md:gap-[35px] mb-[67px] md:mb-[42px] group"
       >
         <div className="flex flex-col gap-5 items-center">
           <h2 className="font-semibold text-[22px] md:text-[40px] md:max-w-[462px]">
